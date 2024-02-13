a34b8c177fae9464e4f62a36d021f9417a37e14a
BellWin98 <jongseunghan@naver.com>
2024-02-13 18:07:59 +0900

    [Practice] vuex의 computed 기능 실습

--- src/components/BasicComponent.vue
+++ src/components/BasicComponent.vue
@@ -11,15 +11,31 @@
   
   <!-- onClick == @click -->
   <button @click="showValue()">변수 변경사항 확인</button>
+
+  <div>
+    <h2>{{ count }}</h2>
+    <button @click="increment">increment</button>
+  </div>
+  <div>
+    <h2>{{ doubleCount }}</h2>
+    <button @click="increment">increment</button>
+  </div>
 </template>
 
 <script>
 export default {  
+  // computed는 종속된 반응형 데이터가 변경될 때만 함수를 다시 실행하여 값을 계산하는 계산 함수
+  computed: {
+    doubleCount(){
+      return this.count * 2;
+    }
+  },
   data(){
     return {
       myLang: "python",
       inputValue1: "python2",
       inputValue2: "python3",
+      count: 0,
     }
   },
   // Vue 생명주기에서 인스턴스가 생성되는 시점을 created라 함
@@ -30,7 +46,10 @@ export default {
   methods: {
     showValue(){
       alert(this.inputValue2);
-    }
+    },
+    increment(){
+      this.count++;
+    },
   }
 }
 </script>
