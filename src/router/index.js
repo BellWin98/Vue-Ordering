5920bc373e3f43de66736642aa13e36b16a18f29
BellWin98 <jongseunghan@naver.com>
2024-02-13 18:10:20 +0900

    [Feat] 상품 등록, 상품 관리 API 및 ItemComponent로 공통화

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 // index.js는 Vue의 Entry Point다.
 
 import { createRouter, createWebHistory } from "vue-router";
-import HomeComponent from '@/components/HomeComponent.vue';
+import ItemList from '@/views/ItemList.vue';
 import LoginComponent from '@/views/LoginComponent.vue';
+import BasicComponent from '@/components/BasicComponent.vue';
+
 // export default인 경우에는 {} 필요없고, 여러개 요소가 있을 경우 {} 필요
 import { memberRoutes } from "./memberRouter.js";
 import { orderRoutes } from "./orderRouter.js";
@@ -12,12 +14,17 @@ const routes = [
     {
         path: '/', // url 경로 지정
         name: 'HOME', // 라우터의 이름
-        component: HomeComponent,
+        component: ItemList,
     },
     {
         path: '/login',
         name: 'Login',
         component: LoginComponent
+    },
+    {
+        path: '/basic',
+        name: 'BasicComponent',
+        component: BasicComponent
     },
     // ...은 스프레드 연산자로 불리며, 주로 배열 요소를 다른 배열 요소에 합할 때 사용
     // memberRoutes의 요소를 가져와서 넣음
